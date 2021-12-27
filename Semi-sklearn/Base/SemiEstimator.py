b94e2f284a9482a4615b04a1fbab6239dc26c58f
21181309jlh <1129198222@qq.com>
2021-12-27 20:18:29 +0800

    modify .gitignore

---
+++ Semi-sklearn/Base/SemiEstimator.py
@@ -0,0 +1,17 @@
+from sklearn.base import BaseEstimator
+from abc import ABC,abstractmethod
+class SemiEstimator(ABC,BaseEstimator):
+    @abstractmethod
+    def fit(self,X,y,unlabled_X,**params):
+        raise NotImplementedError(
+            "Fit method of SemiEstimator class must be implemented."
+        )
+
+
+
+
+
+
+
+
+
