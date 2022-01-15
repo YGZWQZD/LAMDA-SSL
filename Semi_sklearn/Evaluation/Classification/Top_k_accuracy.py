8ee2907c8d0b2998169fc849a4072f92892f2917
21181309jlh <1129198222@qq.com>
2022-01-15 15:33:27 +0800

    Update 2022-01-15

---
+++ Semi_sklearn/Evaluation/Classification/Top_k_accuracy.py
@@ -0,0 +1,5 @@
+from Semi_sklearn.utils import partial
+from Semi_sklearn.Evaluation.Classification.EvaluationClassification import EvaluationClassification
+class Topk_accurary(EvaluationClassification):
+    def __init__(self):
+        
