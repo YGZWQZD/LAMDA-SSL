da36a96cd873b760bfe0215a9d0a0fad3a405b62
21181309jlh <1129198222@qq.com>
2022-02-04 20:44:42 +0800

    add vat

--- Semi_sklearn/Model/Classifier/VAT.py
+++ Semi_sklearn/Model/Classifier/VAT.py
@@ -4,12 +4,15 @@ from Semi_sklearn.Base.SemiDeepModelMixin import SemiDeepModelMixin
 from Semi_sklearn.Opitimizer.SemiOptimizer import SemiOptimizer
 from Semi_sklearn.Scheduler.SemiScheduler import SemiScheduler
 from sklearn.base import ClassifierMixin
+import numpy as np
 from Semi_sklearn.utils import EMA
 import torch
-from Semi_sklearn.utils import cross_entropy
 from Semi_sklearn.utils import class_status
+from Semi_sklearn.utils import partial
 from torch.nn import Softmax
-import math
+from Semi_sklearn.utils import _l2_normalize,kl_div_with_logit,cross_entropy,vat_loss,entropy_loss
+from torch.autograd import Variable
+import torch.nn.functional as F
 
 def fix_bn(m,train=False):
     classname = m.__class__.__name__
@@ -19,7 +22,7 @@ def fix_bn(m,train=False):
         else:
             m.eval()
 
-class UDA(InductiveEstimator,SemiDeepModelMixin,ClassifierMixin):
+class VAT(InductiveEstimator,SemiDeepModelMixin,ClassifierMixin):
     def __init__(self,
                  train_dataset=None,
                  test_dataset=None,
@@ -43,11 +46,14 @@ class UDA(InductiveEstimator,SemiDeepModelMixin,ClassifierMixin):
                  lambda_u=None,
                  mu=None,
                  ema_decay=None,
-                 threshold=0.95,
                  num_classes=None,
                  tsa_schedule=None,
                  weight_decay=None,
-                 T=0.4
+                 eps=6,
+                 warmup=None,
+                 it_vat=1,
+                 xi=1e-6,
+                 lambda_entmin=0.06
                  ):
         SemiDeepModelMixin.__init__(self,train_dataset=train_dataset,
                                     test_dataset=test_dataset,
@@ -72,11 +78,14 @@ class UDA(InductiveEstimator,SemiDeepModelMixin,ClassifierMixin):
                                     )
         self.ema_decay=ema_decay
         self.lambda_u=lambda_u
-        self.threshold=threshold
         self.tsa_schedule=tsa_schedule
         self.num_classes=num_classes
-        self.T=T
         self.weight_decay=weight_decay
+        self.warmup=warmup
+        self.eps=eps
+        self.it_vat=it_vat
+        self.xi=xi
+        self.lambda_entmin=lambda_entmin
         if self.ema_decay is not None:
             self.ema=EMA(model=self._network,decay=ema_decay)
             self.ema.register()
@@ -84,16 +93,13 @@ class UDA(InductiveEstimator,SemiDeepModelMixin,ClassifierMixin):
             self.ema=None
 
         if isinstance(self._augmentation,dict):
-            self.weakly_augmentation=self._augmentation['weakly_augmentation']
-            self.strongly_augmentation = self._augmentation['strongly_augmentation']
+            self.weakly_augmentation=self._augmentation['augmentation']
             self.normalization = self._augmentation['normalization']
         elif isinstance(self._augmentation,(list,tuple)):
             self.weakly_augmentation = self._augmentation[0]
-            self.strongly_augmentation = self._augmentation[1]
-            self.normalization = self._augmentation[2]
+            self.normalization = self._augmentation[1]
         else:
             self.weakly_augmentation = copy.deepcopy(self._augmentation)
-            self.strongly_augmentation = copy.deepcopy(self._augmentation)
             self.normalization = copy.deepcopy(self._augmentation)
 
 
@@ -111,55 +117,65 @@ class UDA(InductiveEstimator,SemiDeepModelMixin,ClassifierMixin):
         if isinstance(self._scheduler,SemiScheduler):
             self._scheduler=self._scheduler.init_scheduler(optimizer=self._optimizer)
 
+
     def start_fit(self):
         self.num_classes = self.num_classes if self.num_classes is not None else \
             class_status(self._train_dataset.labled_dataset.y).num_class
 
     def train(self,lb_X,lb_y,ulb_X,lb_idx=None,ulb_idx=None,*args,**kwargs):
 
-        lb_X=self.weakly_augmentation.fit_transform(copy.deepcopy(lb_X))
-        w_ulb_X=self.weakly_augmentation.fit_transform(copy.deepcopy(ulb_X))
-        s_ulb_X = self.strongly_augmentation.fit_transform(copy.deepcopy(ulb_X))
-        num_lb = lb_X.shape[0]
-        inputs = torch.cat([lb_X, w_ulb_X, s_ulb_X], dim=0)
-        logits = self._network(inputs)
-        logits_x_lb = logits[:num_lb]
-        logits_x_ulb_w, logits_x_ulb_s = logits[num_lb:].chunk(2)
+        _lb_X=self.weakly_augmentation.fit_transform(copy.deepcopy(lb_X))
+        _ulb_X=self.weakly_augmentation.fit_transform(copy.deepcopy(ulb_X))
+        self._network.apply(partial(fix_bn, train=True))
+        logits_x_lb = self._network(_lb_X)
+        # print(torch.any(torch.isnan(logits_x_lb)))
+        self._network.apply(partial(fix_bn, train=False))
+        logits_x_ulb = self._network(_ulb_X)
 
-        return logits_x_lb,lb_y,logits_x_ulb_w, logits_x_ulb_s
+        # print(torch.any(torch.isnan(logits_x_lb)))
+        # print(torch.any(torch.isnan(logits_x_ulb)))
+        d = torch.Tensor(_ulb_X.size()).normal_()
+        for i in range(self.it_vat):
+            d = _l2_normalize(d)
+            d = Variable(d.to(self.device), requires_grad=True)
+            y_hat = self._network(_ulb_X + d)
+            # print(torch.any(torch.isnan(y_hat)))
+            delta_kl = kl_div_with_logit(logits_x_ulb.detach(), y_hat)
+            # print(delta_kl)
+            delta_kl.backward()
+            d = d.grad.data.clone()
+            self._network.zero_grad()
 
-    def get_tsa(self):
-        training_progress = self.it_total / self.num_it_total
+        d = _l2_normalize(d)
+        d = Variable(d)
+        r_adv = self.eps * d
 
-        if self.tsa_schedule is None or self.tsa_schedule == 'none':
-            return 1
-        else:
-            if self.tsa_schedule == 'linear':
-                threshold = training_progress
-            elif self.tsa_schedule == 'exp':
-                scale = 5
-                threshold = math.exp((training_progress - 1) * scale)
-            elif self.tsa_schedule == 'log':
-                scale = 5
-                threshold = 1 - math.exp((-training_progress) * scale)
-            else:
-                raise ValueError('Can not get tsa' )
-            tsa = threshold * (1 - 1 / self.num_classes) + 1 / self.num_classes
-            return tsa
+        y_hat = self._network(_ulb_X + r_adv.detach())
+        # print(torch.any(torch.isnan(y_hat)))
+        self._network.apply(partial(fix_bn,train=False))
+        logits_x_ulb=logits_x_ulb.detach()
+
+        return logits_x_lb,lb_y,logits_x_ulb, y_hat
+
 
 
 
     def get_loss(self,train_result,*args,**kwargs):
-        logits_x_lb,lb_y,logits_x_ulb_w, logits_x_ulb_s=train_result
-        tsa = self.get_tsa()
-        sup_mask = torch.max(torch.softmax(logits_x_lb, dim=-1), dim=-1)[0].le(tsa).float().detach()
-        sup_loss = (cross_entropy(logits_x_lb, lb_y, reduction='none')* sup_mask).mean()  # CE_loss for labeled data
-        pseudo_label = torch.softmax(logits_x_ulb_w, dim=-1)
-        max_probs, max_idx = torch.max(pseudo_label, dim=-1)
-        mask = max_probs.ge(self.threshold).float()
-        pseudo_label = torch.softmax(logits_x_ulb_w / self.T, dim=-1)
-        unsup_loss = (cross_entropy(logits_x_ulb_s, pseudo_label,use_hard_labels=False)*mask ).mean() # MSE loss for unlabeled data
-        loss = sup_loss + self.lambda_u * unsup_loss
+        logits_x_lb,lb_y,logits_x_ulb,y_hat=train_result
+        unsup_warmup = np.clip(self.it_total / (self.warmup * self.num_it_total),
+                a_min=0.0, a_max=1.0)
+
+        sup_loss = cross_entropy(logits_x_lb, lb_y, reduction='mean')
+        # print(sup_loss)
+        unsup_loss = kl_div_with_logit(logits_x_ulb, y_hat)
+        # print(unsup_loss)
+        p = F.softmax(logits_x_ulb, dim=1)
+        entmin_loss=-(p*F.log_softmax(logits_x_ulb, dim=1)).sum(dim=1).mean(dim=0)
+        # print(entmin_loss)
+        loss = sup_loss + self.lambda_u * unsup_loss * unsup_warmup + self.lambda_entmin * entmin_loss
+
+
+
         return loss
 
     def get_predict_result(self,y_est,*args,**kwargs):
--- Semi_sklearn/utils.py
+++ Semi_sklearn/utils.py
@@ -13,6 +13,7 @@ from torch.nn.utils.rnn import PackedSequence
 from collections.abc import Sequence
 from PIL import Image
 import torch.nn.functional as F
+from torch.autograd import Variable
 
 if LooseVersion(sklearn.__version__) >= '0.22.0':
     from sklearn.utils import _safe_indexing as safe_indexing
@@ -363,6 +364,7 @@ def cross_entropy(logits, targets, use_hard_labels=True, reduction='none'):
         nll_loss = torch.sum(-targets * log_pred, dim=1)
         return nll_loss
 
+
 def consistency_loss(logits_w1, logits_w2):
     assert logits_w1.size() == logits_w2.size()
     return F.mse_loss(torch.softmax(logits_w1,dim=-1), torch.softmax(logits_w2,dim=-1), reduction='mean')
@@ -398,5 +400,53 @@ class class_status:
         classes, y_indices = np.unique(self.y_arr, return_inverse=True)
         class_counts = np.bincount(y_indices)
         return class_counts
+
+
+
+def _l2_normalize(d):
+
+    d = d.numpy()
+    d /= (np.sqrt(np.sum(d ** 2, axis=(1, 2, 3))).reshape((-1, 1, 1, 1)) + 1e-16)
+    return torch.from_numpy(d)
+
+def kl_div_with_logit(q_logit, p_logit):
+    q = F.softmax(q_logit, dim=1)
+    logq = F.log_softmax(q_logit, dim=1)
+    logp = F.log_softmax(p_logit, dim=1)
+
+    qlogq = ( q *logq).sum(dim=1).mean(dim=0)
+    qlogp = ( q *logp).sum(dim=1).mean(dim=0)
+    return qlogq - qlogp
+
+def entropy_loss(ul_y):
+    p = F.softmax(ul_y, dim=1)
+    return -(p*F.log_softmax(ul_y, dim=1)).sum(dim=1).mean(dim=0)
+
+def vat_loss(model, ul_x, ul_y, xi=1e-6, eps=6, num_iters=1):
+
+    # find r_adv
+
+    d = torch.Tensor(ul_x.size()).normal_()
+    for i in range(num_iters):
+        d = xi *_l2_normalize(d)
+        d = Variable(d.to('cpu'), requires_grad=True)
+
+        y_hat = model(ul_x + d)
+
+        delta_kl = kl_div_with_logit(ul_y.detach(), y_hat)
+        delta_kl.backward()
+
+        d = d.grad.data.clone().cpu()
+        model.zero_grad()
+
+    d = _l2_normalize(d)
+    d = Variable(d.to('cpu'))
+    r_adv = eps *d
+    # compute lds
+
+    y_hat = model(ul_x + r_adv.detach())
+
+    delta_kl = kl_div_with_logit(ul_y.detach(), y_hat)
+    return delta_kl
 
 
