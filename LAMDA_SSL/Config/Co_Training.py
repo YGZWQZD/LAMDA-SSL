3a5c9e0fb2d6b20db89fb235cf3f98a06f12953f
21181309jlh <1129198222@qq.com>
2022-08-28 13:28:56 +0800

    Improve the robustness of the code

--- LAMDA_SSL/Config/Co_Training.py
+++ LAMDA_SSL/Config/Co_Training.py
@@ -2,10 +2,10 @@ from sklearn.svm import SVC
 random_state=None
 base_estimator = SVC(C=1.0,kernel='rbf',probability=True,gamma='auto')
 base_estimator_2 = SVC(C=1.0,kernel='rbf',probability=True,gamma='auto')
-p = 30
-n = 30
-k = 30
-s = 100
+p = 25
+n = 25
+k = 50
+s = 80
 evaluation = None
 verbose = False
 file = None
