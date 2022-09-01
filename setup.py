ca9e1eb768e41693f03cc471326e71b71b5162ff
21181309jlh <1129198222@qq.com>
2022-09-01 09:24:44 +0800

    Version 1.0

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open("README.md", "r", encoding='utf-8') as fh:
 
 setuptools.setup(
     name='LAMDA-SSL',
-    version="0.0.0.3",
+    version="1.0",
     author="Lin-Han Jia, Lan-Zhe Guo, Zhi Zhou, Yu-Feng Li",
     license='MIT',
     author_email="1129198222@qq.com",
